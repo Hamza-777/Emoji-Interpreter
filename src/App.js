6b41bc50a3653bac898c3fa688c08807f24e907a
Hamza Rarani <hamzararrani77@gmail.com>
2021-07-23 16:49:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState('Interpretation will appear here');
+  const [remark, setRemark] = useState('');
+
+  const emojiObj = {
+    '😀': 'grinning face',
+    '😁': 'beaming face with smiling eyes',
+    '😂': 'face with tears of joy',
+    '🤣': 'rolling on the floor laughing',
+    '😃': 'grinning face with big eyes',
+    '😄': 'grinning face with smiling eyes',
+    '😅': 'grinning face with sweat',
+    '😆': 'grinning squinting face',
+    '😎': 'smiling face with sunglasses',
+    '😝': 'squinting face with tongue',
+    '🤑': 'money-mouth face',
+    '🥶': 'cold face',
+    '😱': 'face screaming in fear',
+    '👹': 'ogre',
+    '👺': 'goblin',
+    '💀': 'skull',
+    '🤡': 'clown face',
+    '👽': 'alien',
+    '👻': 'ghost',
+    '☠': 'skull with crossbones'
+  };
+
+  const emojis = Object.keys(emojiObj);
+
+  const interpretations = Object.values(emojiObj);
+
+  const clickHandler = (e) => {
+    setText(emojiObj[e.target.textContent]);
+  }
+
+  const changeHandler = (e) => {
+    setText('Cannot find an emoji quite like this one.');
+    setRemark(e.target.value);
+    if(interpretations.includes(e.target.value)) {
+      setRemark(emojis[interpretations.indexOf(e.target.value)]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <nav>
+          <h1>Emoji Pedia</h1>
+        </nav>
       </header>
+      <div className='container'>
+        <input type='text' placeholder='Search for an emoji...' onChange={changeHandler} />
+        <p>{remark}</p>
+        <p>{text}</p>
+        <div className='emoji'>
+          {
+            emojis.map(emoji => {
+              return <div className="emojis" onClick={clickHandler}>{emoji}</div>
+            })
+          }
+        </div>
+      </div>
+      <footer>
+        Do you know what your emoji's mean? Well, about time you find out...
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
